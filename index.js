7e8e02af52911ae133c2b5054124bcca36a377f3
Muntaz <you@example.com>
2018-03-28 17:50:58 -0400

    search bar created

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-//create a new component. Index component produce some HTML.
-//webpack and babel do transcribe to the browser 
-const App = function(){
-    return <div> Hi!</div>;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from 'search_bar';
+import videoDetails from 'video_details'
+
+const API_KEY = 'AIzaSyDFQUmWPrFolSelYq0ekwZUYWMaM9Ovtjo';
+
+const App = () => {
+    return ( 
+        <searchBar />
+    </div>
+    );
 }
+
+React.render(<App/>, document.querySelector('.container'));
